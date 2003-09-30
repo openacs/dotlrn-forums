58b5dfe83b68575bc3f808da20b3b6843f87f2a2
Mohan Pakkurti <mohan.pakkurti@gmail.com>
2003-09-30 13:11:40 +0000

    Oracle9i porting changes   - Changed procedure names from delete to del   - Changed view declaration to use actual column names   - Changed "rel_constraints_violated_one" and "rel_constraints_violated_two"     (http://openacs.org/forums/message-view?message_id=81045)

--- sql/oracle/dotlrn-forums-create.sql
+++ sql/oracle/dotlrn-forums-create.sql
@@ -32,6 +32,7 @@ begin
 	foo := acs_sc_impl.new (
 		impl_contract_name => 'dotlrn_applet',
 		impl_name => 'dotlrn_forums',
+		impl_pretty_name => 'dotlrn_forums',
 		impl_owner_name => 'dotlrn_forums'
 	);
 
